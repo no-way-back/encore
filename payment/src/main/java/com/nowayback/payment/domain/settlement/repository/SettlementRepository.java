8fb35c167d72ad9e8709afceb4bdb9522ac2e40e
rnignon <rnignon@naver.com>
2025-12-01 22:49:47 +0900

    feat: Settlement에 대한 Repository 구현

---
+++ payment/src/main/java/com/nowayback/payment/domain/settlement/repository/SettlementRepository.java
@@ -0,0 +1,11 @@
+package com.nowayback.payment.domain.settlement.repository;
+
+import com.nowayback.payment.domain.settlement.entity.Settlement;
+import com.nowayback.payment.domain.settlement.vo.ProjectId;
+
+import java.util.Optional;
+
+public interface SettlementRepository {
+    Settlement save(Settlement settlement);
+    Optional<Settlement> findByProjectId(ProjectId projectId);
+}
---
+++ payment/src/main/java/com/nowayback/payment/infrastructure/settlement/repository/SettlementJpaRepository.java
@@ -0,0 +1,12 @@
+package com.nowayback.payment.infrastructure.settlement.repository;
+
+import com.nowayback.payment.domain.settlement.entity.Settlement;
+import com.nowayback.payment.domain.settlement.vo.ProjectId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SettlementJpaRepository extends JpaRepository<Settlement, UUID> {
+    Optional<Settlement> findByProjectId(ProjectId projectId);
+}

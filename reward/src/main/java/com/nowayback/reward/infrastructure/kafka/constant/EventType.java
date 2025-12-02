bf4bb877057f785417c24e06eebd167d7cf3a971
Cro <junsj1230@naver.com>
2025-12-02 23:38:30 +0900

    refactor: 이벤트 타입 정의

---
+++ reward/src/main/java/com/nowayback/reward/infrastructure/kafka/constant/EventType.java
@@ -0,0 +1,17 @@
+package com.nowayback.reward.infrastructure.kafka.constant;
+
+public enum EventType {
+    PROJECT_CREATED("PROJECT_CREATED"),
+    REWARD_CREATION_SUCCESS("REWARD_CREATION_SUCCESS"),
+    REWARD_CREATION_FAILED("REWARD_CREATION_FAILED");
+
+    private final String value;
+
+    EventType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}

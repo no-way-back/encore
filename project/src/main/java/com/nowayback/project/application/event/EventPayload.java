74238ba981fc4585baec1cdbef7565be37543cab
sejunO <friendlyoh@gmail.com>
2025-12-02 18:32:44 +0900

    feat: 이벤트 처리 구조를 위한 인터페이스 정의

---
+++ project/src/main/java/com/nowayback/project/application/event/EventPayload.java
@@ -0,0 +1,4 @@
+package com.nowayback.project.application.event;
+
+public interface EventPayload {
+}
--- project/src/main/java/com/nowayback/project/application/outbox/event/OutboxEventPublisher.java
+++ project/src/main/java/com/nowayback/project/application/outbox/event/OutboxEventPublisher.java
@@ -1,6 +1,8 @@
 package com.nowayback.project.application.outbox.event;
 
-import com.nowayback.project.application.outbox.event.payload.EventPayload;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nowayback.project.application.event.EventPayload;
 import com.nowayback.project.domain.outbox.Outbox;
 import com.nowayback.project.domain.outbox.vo.AggregateType;
 import com.nowayback.project.domain.outbox.vo.EventType;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OutboxEventPublisher {
 
+    private final ObjectMapper objectMapper;
     private final ApplicationEventPublisher applicationEventPublisher;
 
     public void publish(EventType type, EventPayload payload, AggregateType aggregateType, UUID aggregateId) {
@@ -25,9 +28,17 @@ public class OutboxEventPublisher {
             aggregateType,
             aggregateId,
             type,
-            payload.toJson()
+            toJson(payload)
         );
 
         applicationEventPublisher.publishEvent(OutboxEvent.of(outbox));
+    }
+
+    private String toJson(Object payload) {
+        try {
+            return objectMapper.writeValueAsString(payload);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("[EventPayload.toJson] EventPayload 직렬화에 실패했습니다.", e);
+        }
     }
 }

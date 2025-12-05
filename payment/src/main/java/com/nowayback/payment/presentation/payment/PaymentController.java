c3b3deee449e063a8f2568af214d2e425c9bb72d
rnignon <rnignon@naver.com>
2025-12-05 11:41:28 +0900

    feat: 결제 목록 조회 컨트롤러 코드 구현

--- payment/src/main/java/com/nowayback/payment/presentation/payment/PaymentController.java
+++ payment/src/main/java/com/nowayback/payment/presentation/payment/PaymentController.java
@@ -3,12 +3,18 @@ package com.nowayback.payment.presentation.payment;
 import com.nowayback.payment.application.payment.PaymentService;
 import com.nowayback.payment.application.payment.dto.command.ConfirmPaymentCommand;
 import com.nowayback.payment.application.payment.dto.command.RefundPaymentCommand;
+import com.nowayback.payment.application.payment.dto.result.PaymentResult;
+import com.nowayback.payment.infrastructure.auth.user.AuthUser;
+import com.nowayback.payment.infrastructure.auth.user.CurrentUser;
+import com.nowayback.payment.presentation.dto.response.PageResponse;
 import com.nowayback.payment.presentation.payment.dto.request.ConfirmPaymentRequest;
 import com.nowayback.payment.presentation.payment.dto.request.RefundPaymentRequest;
 import com.nowayback.payment.presentation.payment.dto.response.ConfirmPaymentResponse;
+import com.nowayback.payment.presentation.payment.dto.response.PaymentResponse;
 import com.nowayback.payment.presentation.payment.dto.response.RefundPaymentResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +27,20 @@ import java.util.UUID;
 public class PaymentController {
 
     private final PaymentService paymentService;
+
+    @GetMapping
+    public ResponseEntity<PageResponse<PaymentResponse>> getPayments(
+            @CurrentUser AuthUser authUser,
+            @RequestParam(required = false) UUID userId,
+            @RequestParam(required = false) UUID projectId,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size
+    ) {
+        Page<PaymentResult> results = paymentService.getPayments(userId, projectId, page, size, authUser.userId(), authUser.role());
+
+        return ResponseEntity.ok(PageResponse.from(results.map(PaymentResponse::from)));
+    }
+
 
     @PostMapping("/confirm")
     public ResponseEntity<ConfirmPaymentResponse> confirmPayment(

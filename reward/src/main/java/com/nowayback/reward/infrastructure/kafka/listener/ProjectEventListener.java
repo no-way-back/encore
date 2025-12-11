a510cdbc197216cc148417e695c30658c270f5a1
Cro <junsj1230@naver.com>
2025-12-11 11:33:37 +0900

    refactor: 카프카 컨슈머 팩토리 이름 변경

--- reward/src/main/java/com/nowayback/reward/infrastructure/kafka/listener/ProjectEventListener.java
+++ reward/src/main/java/com/nowayback/reward/infrastructure/kafka/listener/ProjectEventListener.java
@@ -27,7 +27,7 @@ public class ProjectEventListener {
     @KafkaListener(
             topics = "${spring.kafka.topic.project-reward-creation}",
             groupId = "${spring.kafka.consumer.group-id}",
-            containerFactory = "projectCreatedConsumerFactory"
+            containerFactory = "projectCreatedListenerFactory"
     )
     public void consumeProjectEvent(
             @Payload ProjectCreatedEvent event,

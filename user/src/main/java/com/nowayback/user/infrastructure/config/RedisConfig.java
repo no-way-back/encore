ad7c801da9b1847ad3dc05879990e0d4a02176f7
rnignon <rnignon@naver.com>
2025-12-15 17:19:18 +0900

    chore: 유저 마이크로 서비스 내 RedisConfig 수정

--- user/src/main/java/com/nowayback/user/infrastructure/config/RedisConfig.java
+++ user/src/main/java/com/nowayback/user/infrastructure/config/RedisConfig.java
@@ -1,8 +1,11 @@
 package com.nowayback.user.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -11,9 +14,28 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.ssl.enabled:false}")
+    private boolean redisSslEnabled;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl()
+                .build();
+
+        if (!redisSslEnabled) {
+            clientConfig = LettuceClientConfiguration.builder().build();
+        }
+
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
     }
 
     @Bean

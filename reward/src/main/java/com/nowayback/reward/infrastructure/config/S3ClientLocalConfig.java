ebb6cb7df9b673bf025650d3d334e7f721409e98
Cro <junsj1230@naver.com>
2025-12-15 21:05:49 +0900

    refcator: aws access key 설정 변경

--- reward/src/main/java/com/nowayback/reward/infrastructure/config/S3ClientLocalConfig.java
+++ reward/src/main/java/com/nowayback/reward/infrastructure/config/S3ClientLocalConfig.java
@@ -3,13 +3,15 @@ package com.nowayback.reward.infrastructure.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
-public class S3Config {
+@Profile("local")
+public class S3ClientLocalConfig {
 
     @Value("${spring.cloud.aws.s3.access-key}")
     private String accessKey;
@@ -22,11 +24,13 @@ public class S3Config {
 
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
-
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
                 .build();
     }
 }

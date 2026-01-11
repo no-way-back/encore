e4368ebb155c1844306f518eff2ff16d266b8bc8
Cro <junsj1230@naver.com>
2026-01-12 00:05:17 +0900

    refactor: Redisson 활용 분산 락 설정

--- reward/src/main/java/com/nowayback/reward/infrastructure/repository/reward/persistence/RewardJpaRepository.java
+++ reward/src/main/java/com/nowayback/reward/infrastructure/repository/reward/persistence/RewardJpaRepository.java
@@ -2,9 +2,7 @@ package com.nowayback.reward.infrastructure.repository.reward.persistence;
 
 import com.nowayback.reward.domain.reward.entity.Rewards;
 import com.nowayback.reward.domain.vo.ProjectId;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,10 +12,18 @@ import java.util.UUID;
 
 public interface RewardJpaRepository extends JpaRepository<Rewards, UUID> {
 
-    @Query("SELECT r FROM Rewards r JOIN FETCH r.optionList WHERE r.projectId = :projectId AND r.status = 'AVAILABLE'")
+    @Query("""
+        SELECT r FROM Rewards r
+        JOIN FETCH r.optionList
+        WHERE r.projectId = :projectId
+        AND r.status = 'AVAILABLE'
+        """)
     List<Rewards> findAvailableReward(ProjectId projectId);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT r FROM Rewards r WHERE r.id = :id")
-    Optional<Rewards> findByIdWithLock(@Param("id") UUID id);
+    @Query("""
+        SELECT r FROM Rewards r
+        LEFT JOIN FETCH r.optionList
+        WHERE r.id = :id
+        """)
+    Optional<Rewards> findByIdWithOptions(@Param("id") UUID id);
 }
